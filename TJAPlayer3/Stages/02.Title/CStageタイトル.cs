666368e884e3f05522f846fe1f1c85b921a55c3b
akki0716 <eggeeja@gmail.com>
2020-05-02 22:07:16 +0900

    署名を変更、バージョン表記を変更

--- TJAPlayer3/Stages/02.Title/CStageタイトル.cs
+++ TJAPlayer3/Stages/02.Title/CStageタイトル.cs
@@ -187,14 +187,14 @@ namespace TJAPlayer3
                 if (TJAPlayer3.Tx.Title_Background != null )
                     TJAPlayer3.Tx.Title_Background.t2D描画( TJAPlayer3.app.Device, 0, 0 );
 
-                #region[ バージョン表示 ]
-                //string strVersion = "KTT:J:A:I:2017072200";
-                string strCreator = "https://github.com/AioiLight/TJAPlayer3";
+				#region[ バージョン表示 ]
+				//string strVersion = "KTT:J:A:I:2017072200";
+				string strCreator = "https://github.com/akki0716/TJAPlayer3";
                 AssemblyName asmApp = Assembly.GetExecutingAssembly().GetName();
 #if DEBUG
                 TJAPlayer3.act文字コンソール.tPrint(4, 44, C文字コンソール.Eフォント種別.白, "DEBUG BUILD");
 #endif
-                TJAPlayer3.act文字コンソール.tPrint(4, 4, C文字コンソール.Eフォント種別.白, asmApp.Name + " Ver." + TJAPlayer3.VERSION + " (" + strCreator + ")" );
+                TJAPlayer3.act文字コンソール.tPrint(4, 4, C文字コンソール.Eフォント種別.白, asmApp.Name + " Ver." + TJAPlayer3.VERSION + " " + "MOD"  + " (" + strCreator + ")" );
                 TJAPlayer3.act文字コンソール.tPrint(4, 24, C文字コンソール.Eフォント種別.白, "Skin:" + TJAPlayer3.Skin.Skin_Name + " Ver." + TJAPlayer3.Skin.Skin_Version + " (" + TJAPlayer3.Skin.Skin_Creator + ")");
                 //CDTXMania.act文字コンソール.tPrint(4, 24, C文字コンソール.Eフォント種別.白, strSubTitle);
                 TJAPlayer3.act文字コンソール.tPrint(4, (720 - 24), C文字コンソール.Eフォント種別.白, "TJAPlayer3 forked TJAPlayer2 forPC(kairera0467)");
